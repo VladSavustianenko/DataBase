b600e7cf35e4dedda7b51fc9529b29a2adee218b
VladSavustianenko <44864120+VladSavustianenko@users.noreply.github.com>
2020-05-15 19:00:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 
-username = 'VladDB'
+username = '"VladDB"'
 password = 'Vlad03042001'
 database = 'localhost:1521/xe'
 
@@ -8,17 +8,17 @@ connection = cx_Oracle.connect(username, password, database)
 
 cursor = connection.cursor()
 
-print("Кількість забитих голів кожною командою.\n")
+print("Кількість голів, які забили команди за всі сезони.\n")
 query1 ="""
 SELECT
     TEAM, sum(GOALS)
-        FROM (SELECT "EPL".HOME_TEAM as TEAM, sum("EPL".HOME_GOALS) as GOALS from EPL 
-        GROUP BY "EPL".HOME_TEAM
+        FROM (SELECT MATCH.TEAM_HOME_NAME as TEAM, sum(MATCH.MATCH_HOME_GOALS) as GOALS from MATCH 
+        GROUP BY MATCH.TEAM_HOME_NAME
         UNION
-        SELECT "EPL".AWAY_TEAM as TEAM, sum("EPL".AWAY_GOALS) as GOALS from EPL 
-        GROUP BY "EPL".AWAY_TEAM)
+        SELECT MATCH.TEAM_AWAY_NAME as TEAM, sum(MATCH.MATCH_AWAY_GOALS) as GOALS from MATCH 
+        GROUP BY MATCH.TEAM_AWAY_NAME)
 GROUP BY TEAM
-ORDER BY sum(GOALS) DESC;
+ORDER BY sum(GOALS) DESC
 """
 cursor.execute(query1)
 
@@ -29,8 +29,8 @@ for row in cursor:
 
 print("Кількість нічиїх, перемог та поразок.\n")
 query2 = """
-SELECT count(*) FROM "EPL"
-GROUP BY "EPL".RESULTS;
+SELECT MATCH.MATCH_RESULTS, count(*) FROM MATCH
+GROUP BY MATCH.MATCH_RESULTS
 """
 cursor.execute(query2)
 
@@ -39,12 +39,12 @@ for row in cursor:
 
 
 
-print("Кількість нічиїх в кожному сезоні.\n")
+print("Загальна кількість нічиїх усіх команд за кожен сезон.\n")
 query3 = """
-SELECT "EPL".SEASON, count(*) FROM "EPL"
-WHERE "EPL".RESULTS = 'H'
-GROUP BY "EPL".SEASON
-ORDER BY "EPL".SEASON;
+SELECT MATCH.SEASON_PERIOD, count(*) FROM MATCH
+WHERE MATCH.MATCH_RESULTS = 'D'
+GROUP BY MATCH.SEASON_PERIOD
+ORDER BY MATCH.SEASON_PERIOD
 """
 cursor.execute(query3)
 

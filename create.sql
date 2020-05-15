7a6260a197a050bef098915ed53fe2080de584b0
VladSavustianenko <44864120+VladSavustianenko@users.noreply.github.com>
2020-05-15 18:58:06 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,16 +1,43 @@
-CREATE TABLE EPL 
+/*==============================================================*/
+/* Table: MATCH                                                 */
+/*==============================================================*/
+create table MATCH 
 (
-  HOME_TEAM VARCHAR2(50) NOT NULL, 
-  AWAY_TEAM VARCHAR2(50) NOT NULL, 
-  HOME_GOALS NUMBER NOT NULL,
-  AWAY_GOALS NUMBER NOT NULL,
-  RESULTS CHAR NOT NULL,
-  SEASON VARCHAR2(9) NOT NULL, 
-  CONSTRAINT EPL_PK PRIMARY KEY 
-  (
-    HOME_TEAM, 
-	AWAY_TEAM, 
-	SEASON 
-  )
-  ENABLE 
+   TEAM_HOME_NAME       VARCHAR2(50)         not null,
+   TEAM_AWAY_NAME       VARCHAR2(50)         not null,
+   MATCH_HOME_GOALS     NUMBER,
+   MATCH_AWAY_GOALS     NUMBER,
+   MATCH_RESULTS        CHAR(1),
+   SEASON_PERIOD        VARCHAR2(20)         not null,
+   constraint PK_MATCH primary key (TEAM_HOME_NAME, TEAM_AWAY_NAME, SEASON_PERIOD)
 );
+
+/*==============================================================*/
+/* Table: SEASON                                                */
+/*==============================================================*/
+create table SEASON 
+(
+   SEASON_PERIOD        VARCHAR2(20)         not null,
+   constraint PK_SEASON primary key (SEASON_PERIOD)
+);
+
+/*==============================================================*/
+/* Table: TEAM                                                  */
+/*==============================================================*/
+create table TEAM 
+(
+   TEAM_NAME            VARCHAR2(50)         not null,
+   constraint PK_TEAM primary key (TEAM_NAME)
+);
+
+alter table MATCH
+   add constraint FK_MATCH_M_T_1_TEAM foreign key (TEAM_HOME_NAME)
+      references TEAM (TEAM_NAME);
+
+alter table MATCH
+   add constraint FK_MATCH_M_T_2_TEAM foreign key (TEAM_AWAY_NAME)
+      references TEAM (TEAM_NAME);
+
+alter table MATCH
+   add constraint FK_MATCH_RELATIONS_SEASON foreign key (SEASON_PERIOD)
+      references SEASON (SEASON_PERIOD);

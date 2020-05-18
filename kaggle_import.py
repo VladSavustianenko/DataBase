46e7cd2041d4128c59b7a694f928435edf0d1012
VladSavustianenko <44864120+VladSavustianenko@users.noreply.github.com>
2020-05-18 17:47:00 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -3,10 +3,8 @@ import cx_Oracle
 username = '"VladDB"'
 password = 'Vlad03042001'
 database = 'localhost:1521/xe'
-connection = cx_Oracle.connect(username,password, database)
+connection = cx_Oracle.connect('"Vano"','12341234', database)
 cursor = connection.cursor()
-
-
 
 
 csv_file="results.csv"
@@ -20,14 +18,13 @@ with open(csv_file, newline='') as file:
         for row in reader:
             home_team = row['home_team']
             away_team = row['away_team']
-            home_goals = row['home_goals']
-			away_goals = row['away_goals']
-			result = row['result']
-			season = row['season']
+            home_goals = int(float(row['home_goals']))
+            away_goals = int(float(row['away_goals']))
+            result = row['result']
+            season = row['season']
             
 
-            insert = """ INSERT INTO Match ( home_team, away_team, home_goals, away_goals, result, season)
-                values (:home_team, :away_team, :home_goals, :away_goals, :result, :season)"""
+            insert = """INSERT INTO Match ( TEAM_HOME_NAME, TEAM_AWAY_NAME, MATCH_HOME_GOALS, MATCH_AWAY_GOALS, MATCH_RESULTS, SEASON_PERIOD)values (:home_team, :away_team, :home_goals, :away_goals, :result, :season)"""
             cursor.execute(insert, home_team=home_team, away_team=away_team, home_goals=home_goals, away_goals=away_goals, result=result, season=season)
             
 

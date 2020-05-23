580a5b5799584649d3d96cf2e99d56dab9cbde5a
VladSavustianenko <44864120+VladSavustianenko@users.noreply.github.com>
2020-05-23 18:15:04 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -8,6 +8,5 @@ BEGIN
         VALUES ('Barcelona', 'Liverpool', '5', '0', 'H', '2010-2011');
         INSERT INTO match(team_home_name, team_away_name, match_home_goals, match_away_goals, match_results, season_period)
         VALUES ('Liverpool', 'Barcelona', '1', '3', 'A', '2017-2018');
-        
     END LOOP;
 END;
